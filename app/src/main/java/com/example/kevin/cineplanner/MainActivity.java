e27f99b206c865723979f12edfb3564b1fbf7361
KevinVi <kevin.vivor@gmail.com>
2017-09-17 18:05:37 +0200

    creation app android

---
+++ app/src/main/java/com/example/kevin/cineplanner/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.kevin.cineplanner;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int TIME_LOADER = 2000;
+    private final int TIME_LOADER_FAIL = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+            }
+        }, TIME_LOADER);
+    }
+}
